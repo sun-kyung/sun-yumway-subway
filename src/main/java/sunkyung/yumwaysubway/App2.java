68536668251b79a2613648698feed4e5d59398f7
sun-kyung <esun1801@naver.com>
2019-12-18 18:14:05 +0900

    v001

---
+++ src/main/java/sunkyung/yumwaysubway/App2.java
@@ -0,0 +1,14 @@
+package sunkyung.yumwaysubway;
+
+public class App2 {
+  public static void main(String[] args) {
+    System.out.println("번호: 1");
+    System.out.println("이름: 이선경");
+    System.out.println("이메일: esun1801@gmail.com");
+    System.out.println("암호: 1111");
+    System.out.println("사진: hong.png");
+    System.out.println("전화: 010-5382-1770");
+    System.out.println("가입일: 2019-12-10");
+  }
+
+}
---
+++ src/main/java/sunkyung/yumwaysubway/App3.java
@@ -0,0 +1,11 @@
+package sunkyung.yumwaysubway;
+
+public class App3 {
+  public static void main(String[] args) {
+    System.out.println("글제목: 서브웨이는 맛있어요");
+    System.out.println("내용: 매일 먹어도 질리지 않아요.");
+    System.out.println("작성일: 2019-12-10");
+    System.out.println("조회수: 0");
+  }
+
+}
